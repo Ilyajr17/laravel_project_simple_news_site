764004f2ee8c9b93504a4dbecc5645433a013697
Ilya Zherebtsov <izherebcov@gmail.com>
2022-07-12 17:07:03 +0400

    добавление мидлваров

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,18 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login_process', [AuthController::class, 'login'])->name('login_process');
 
-Route::post('/login_process', [AuthController::class, 'login'])->name('login_process');
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register_process', [AuthController::class, 'register'])->name('register_process');
+});
 
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 
-Route::post('/register_process', [AuthController::class, 'register'])->name('register_process');
+
+
